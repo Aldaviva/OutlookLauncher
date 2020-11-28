6ebc2f66f60292b7f909ac5af3226699229a9df6
Ben Hutchison <ben@aldaviva.com>
2020-11-27 17:22:44 -0800

    Add project files.

---
+++ OutlookLauncher/Program.cs
@@ -0,0 +1,46 @@
+﻿#nullable enable
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using ManagedWinapi.Windows;
+using Microsoft.Win32;
+
+namespace OutlookLauncher {
+
+    internal static class Program {
+
+        private static void Main() {
+            if (findOutlookWindow() is {} outlookWindow) {
+                if (outlookWindow.WindowState == FormWindowState.Minimized) {
+                    Win32.ShowWindow(outlookWindow.HWnd, Win32.ShowWindowCommand.RESTORE);
+                }
+
+                if (!outlookWindow.TopMost) {
+                    outlookWindow.TopMost = true;
+                    outlookWindow.TopMost = false;
+                }
+            } else if (getOutlookExecutableAbsolutePath() is {} executablePath) {
+                Process.Start(executablePath)?.Dispose();
+            } else {
+                MessageBox.Show("Outlook installation not found.", "Failed to start Outlook", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static SystemWindow? findOutlookWindow() => SystemWindow.FilterToplevelWindows(candidate =>
+            candidate.ClassName == "rctrl_renwnd32" &&
+            candidate.Title.EndsWith(" - Outlook") &&
+            "outlook".Equals(candidate.Process.ProcessName, StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+
+        private static string? getOutlookExecutableAbsolutePath() {
+            if (Registry.GetValue(@"HKEY_CLASSES_ROOT\stssync\shell\open\command", null, null) is string commandValue) {
+                return commandValue.Split(new[] { " /share " }, 2, StringSplitOptions.None)[0];
+            } else {
+                return null;
+            }
+        }
+
+    }
+
+}
---
+++ OutlookLauncher/Win32.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace OutlookLauncher {
+
+    internal static class Win32 {
+
+        [DllImport("user32.dll")]
+        internal static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommand nCmdShow);
+
+        internal enum ShowWindowCommand {
+
+            HIDE               = 0,
+            NORMAL             = 1,
+            SHOW_MINIMIZED     = 2,
+            MAXIMIZE           = 3,
+            SHOW_NO_ACTIVATE   = 4,
+            SHOW               = 5,
+            MINIMIZE           = 6,
+            SHOW_MIN_NO_ACTIVE = 7,
+            SHOW_NA            = 8,
+            RESTORE            = 9,
+            SHOW_DEFAULT       = 10,
+            FORCE_MINIMIZE     = 11
+
+        }
+
+    }
+
+}
